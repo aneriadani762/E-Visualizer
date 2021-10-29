51f05e7b30ab152b86757fd3be727b6c0cd367e3
Aneri Adani <aneriadani12345@gmail.com>
2021-10-29 23:29:18 +0530

    updated index.js

--- shapes-quiz/index.js
+++ shapes-quiz/index.js
@@ -80,3 +80,74 @@ const quizShapes = [
         ans: "ans2" 
     }
 ];
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit = document.querySelector('#submit');
+
+const answers = document.querySelectorAll('.answer');
+
+const showScore = document.querySelector('#showScore');
+
+let questionCount = 0;
+let score = 0;
+
+const loadQuestion = ( ) => {
+    const questionList = quizShapes[questionCount];
+
+    question.innerText = questionList.question;
+
+    option1.innerText = questionList.a;
+    option2.innerText = questionList.b;
+    option3.innerText = questionList.c;
+    option4.innerText = questionList.d;
+}
+
+loadQuestion();
+
+const getCheckAnswer = () => {
+    let answer;
+
+    answers.forEach((curAnsElem) => {
+        if(curAnsElem.checked){
+            answer = curAnsElem.id;
+        }
+    });
+
+    return answer;
+};
+
+const deselectAll = () => {
+    answers.forEach((curAnsElem) => curAnsElem.checked = false );
+}
+
+submit.addEventListener('click', () => {
+    const checkedAnswer = getCheckAnswer();
+    console.log(checkedAnswer);
+
+    if(checkedAnswer === quizShapes[questionCount].ans){
+        score++;
+    };
+
+    questionCount++;
+
+    deselectAll();
+
+    if(questionCount < quizShapes.length){
+        loadQuestion();
+    }
+    else{
+
+        showScore.innerHTML = `
+
+        <h3> You scored ${score}/${quizShapes.length} ✌️</h3>
+        <button class="btn" onclick="Location.reload()">Play Again!</button>
+
+        `;
+
+        showScore.classList.remove('scoreArea');
+    }
+});
