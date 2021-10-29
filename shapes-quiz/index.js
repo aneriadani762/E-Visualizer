43566c88a0efa563cb035824d24849709d262098
Aneri Adani <aneriadani12345@gmail.com>
2021-10-29 21:33:38 +0530

    added shapes-quiz

---
+++ shapes-quiz/index.js
@@ -0,0 +1,82 @@
+const quizShapes = [
+    {
+        question: "Q1 : A  solid object has 6 faces, 12 edges and 8 corners. This object could be a...?",
+        a: "Cylinder",
+        b: "Cuboid",
+        c: "Cone",
+        d: "Sphere",
+        ans: "ans2" 
+    },
+    {
+        question: "Q2 : How many faces does a Cube have?",
+        a: "6",
+        b: "8",
+        c: "4",
+        d: "12",
+        ans: "ans1" 
+    },
+    {
+        question: "Q3 : A perfectly round solid is called a...?",
+        a: "Cylinder",
+        b: "Cuboid",
+        c: "Cone",
+        d: "Sphere",
+        ans: "ans4" 
+    },
+    {
+        question: "Q4 : How many flat faces does a Cylinder have? ",
+        a: "2",
+        b: "0",
+        c: "3",
+        d: "6",
+        ans: "ans1" 
+    },
+    {
+        question: "Q5 : How many edges does a cube have? ",
+        a: "8",
+        b: "4",
+        c: "6",
+        d: "12",
+        ans: "ans2" 
+    },
+    {
+        question: "Q6 : What 3D shape looks like a book?",
+        a: "Cylinder",
+        b: "Cuboid",
+        c: "Cone",
+        d: "Sphere",
+        ans: "ans2" 
+    },
+    {
+        question: "Q7 : How many faces does a Sphere have?",
+        a: "0",
+        b: "2",
+        c: "1",
+        d: "4",
+        ans: "ans3" 
+    },
+    {
+        question: "Q8 : What 3D shape looks like a ball?",
+        a: "Cylinder",
+        b: "Cuboid",
+        c: "Cone",
+        d: "Sphere",
+        ans: "ans4" 
+    },
+    {
+        question: "Q9 : Which 3D shape has all equal edges?",
+        a: "Cylinder",
+        b: "Cube",
+        c: "Cone",
+        d: "Sphere",
+        ans: "ans2" 
+    },
+    {
+        question: "Q10 : How many vertices does a Cone have?",
+        a: "0",
+        b: "1",
+        c: "2",
+        d: "3",
+        ans: "ans2" 
+    }
+];
